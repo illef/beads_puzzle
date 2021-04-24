974e31e326a67f168f36dfb8923ff926ec1afe85
illef <sai.illef@gmail.com>
2021-04-24 21:48:43 +0900

    Add zinuns problem

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,108 @@ fn number_62() {
     }
 }
 
+fn zinun_problem_1() {
+    let map = Map::new_with_fill(6, 10, vec![(4, 0), (5, 0), (5, 1), (5, 2), (5, 3)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B5));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+}
+
+fn zinun_problem_2() {
+    let map = Map::new_with_fill(6, 10, vec![(2, 4), (3, 4), (4, 4), (4, 5), (4, 6)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B4));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+}
+
+fn zinun_problem_3() {
+    let map = Map::new_with_fill(6, 10, vec![(3, 0), (2, 1), (3, 1), (4, 1), (3, 2)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B7));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+}
+
+fn zinun_problem_4() {
+    let map = Map::new_with_fill(6, 10, vec![(2, 0), (3, 0), (1, 1), (2, 1), (1, 2)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B11));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+}
+
+fn zinun_problem_5() {
+    let map = Map::new_with_fill(6, 10, vec![(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B9));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+}
+
+fn zinun_problem_6() {
+    let map = Map::new_with_fill(6, 10, vec![(1, 0), (1, 1), (2, 1), (3, 1), (3, 2)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B6));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    }
+    else {
+        println!("답이 없어요!!!");
+    }
+}
+
+fn illef_p1() {
+    let map = Map::new_with_fill(6, 10, vec![(1, 0), (2, 0), (0, 1), (1, 1), (2, 1)]);
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+    blocks.remove(&Block::new(init::B10));
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    } else {
+        println!("답이 없어요!!!");
+    }
+}
+
+fn illef_p2() {
+    let map = Map::default();
+    println!("{}", map);
+
+    let mut blocks = Block::get_all();
+
+    if let Some(answer) = solve(map, blocks) {
+        println!("{}", answer);
+    } else {
+        println!("답이 없어요!!!");
+    }
+}
+
 fn main() {
-    number_62();
+    zinun_problem_6();
 }
